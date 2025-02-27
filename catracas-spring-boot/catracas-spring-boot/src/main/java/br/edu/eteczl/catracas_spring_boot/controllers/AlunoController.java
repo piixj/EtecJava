ba46a494be56418854e1763b0bbec3d527f43be0
piixj <kauansouza2026@gmail.com>
2025-02-27 20:41:01 -0300

    primeirorest

--- catracas-spring-boot/catracas-spring-boot/src/main/java/br/edu/eteczl/catracas_spring_boot/controllers/AlunoController.java
+++ catracas-spring-boot/catracas-spring-boot/src/main/java/br/edu/eteczl/catracas_spring_boot/controllers/AlunoController.java
@@ -1,16 +1,15 @@
 package br.edu.eteczl.catracas_spring_boot.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/alunos")
+@RequestMapping("/api/v1/aluno")
 public class AlunoController {
     
     @RequestMapping("/get")
-    public String getAluno(@RequestParam String nome) {
-        return " Olá Aluno " + nome;
+    public String getAluno() {
+        return " Olá Aluno ";
     }
 
 }
