8a7b1b545d18bec910f4b3fe8bc54531808147c9
piixj <kauansouza2026@gmail.com>
2025-03-19 15:25:50 -0300

    quase finalizando o spring

--- demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
+++ demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
@@ -2,16 +2,25 @@ package com.example.demo.restservices;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Valid;
+import jakarta.validation.Validator;
+
 @RestController
 @RequestMapping("/api/v1")
 public class AlunosRestServices {
+
+    @Autowired
+    Validator validator;
 
     @GetMapping("/alunos")
     public List<Aluno> getAlunos() {
@@ -24,6 +33,9 @@ public class AlunosRestServices {
 
     @PostMapping("/alunos")
     public Aluno inserirAluno(@RequestBody Aluno aluno) {
+        Set<ConstraintViolation<Aluno>> validate = validator.validate(aluno);
+        
+        System.out.println("Aluno inserido: " + aluno);
         return aluno;
     }
 }
