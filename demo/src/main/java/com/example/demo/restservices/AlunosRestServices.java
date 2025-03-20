8a4aeacd1ed93f07b937480ccc0b198702638f8f
piixj <kauansouza2026@gmail.com>
2025-03-19 23:40:47 -0300

    Springboot

--- demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
+++ demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
@@ -12,12 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Aluno;
 
-<<<<<<< HEAD
-import jakarta.validation.Validator;
-=======
 import jakarta.validation.Valid;
-
->>>>>>> e0d55935a91e0f594389a65713e1223a38ab546b
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,24 +23,14 @@ public class AlunosRestServices {
     @GetMapping("/alunos")
     public List<Aluno> getAlunos() {
         List<Aluno> alunos = new ArrayList<>();
-<<<<<<< HEAD
         alunos.add(new Aluno("João15", 15, "468.697.280-97"));
         alunos.add(new Aluno("João16", 16, "186.177.280-78"));
         alunos.add(new Aluno("João17", 17, "580.032.490-58"));
-=======
-        alunos.add(new Aluno("João15", 15, "sasa"));
-        alunos.add(new Aluno("João16", 16, "sas"));
-        alunos.add(new Aluno("João17", 17, "asassa"));
->>>>>>> e0d55935a91e0f594389a65713e1223a38ab546b
         return alunos;
     }
 
     @PostMapping("/alunos")
-<<<<<<< HEAD
-    public Aluno inserirAluno(@RequestBody Aluno aluno) {        
-=======
     public Aluno inserirAluno(@RequestBody @Valid Aluno aluno) {        
->>>>>>> e0d55935a91e0f594389a65713e1223a38ab546b
         System.out.println("Aluno inserido: " + aluno);
         return aluno;
     }
