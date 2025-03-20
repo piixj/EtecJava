c4ad912194f02bd93b8e7498dd53d1250d5dfee8
piixj <kauansouza2026@gmail.com>
2025-03-19 23:19:27 -0300

    Spring

--- demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
+++ demo/src/main/java/com/example/demo/restservices/AlunosRestServices.java
@@ -2,7 +2,6 @@ package com.example.demo.restservices;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Valid;
+import com.example.demo.model.Aluno;
+
 import jakarta.validation.Validator;
 
 @RestController
@@ -25,16 +24,14 @@ public class AlunosRestServices {
     @GetMapping("/alunos")
     public List<Aluno> getAlunos() {
         List<Aluno> alunos = new ArrayList<>();
-        alunos.add(new Aluno("João15", 15));
-        alunos.add(new Aluno("João16", 16));
-        alunos.add(new Aluno("João17", 17));
+        alunos.add(new Aluno("João15", 15, "468.697.280-97"));
+        alunos.add(new Aluno("João16", 16, "186.177.280-78"));
+        alunos.add(new Aluno("João17", 17, "580.032.490-58"));
         return alunos;
     }
 
     @PostMapping("/alunos")
-    public Aluno inserirAluno(@RequestBody Aluno aluno) {
-        Set<ConstraintViolation<Aluno>> validate = validator.validate(aluno);
-        
+    public Aluno inserirAluno(@RequestBody Aluno aluno) {        
         System.out.println("Aluno inserido: " + aluno);
         return aluno;
     }
